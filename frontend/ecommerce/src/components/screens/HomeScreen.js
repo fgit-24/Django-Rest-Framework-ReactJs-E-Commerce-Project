c5a467639a9b909d5778c56e31fad2a1bd764120
Florian Hass <f91hama@gmail.com>
2024-09-02 12:41:50 +0000

    Add useeffect function to homescreen.js

--- frontend/ecommerce/src/components/screens/HomeScreen.js
+++ frontend/ecommerce/src/components/screens/HomeScreen.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 function Homescreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const { data } = await axios.get('/api/products/');
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    }
+    fetchProducts();
+  }, []);
+
   return (
-
-    <div>Homescreen</div>
-  )
-
+    <Container>
+      <br />
+      <h1>Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} xs={12} md={6} lg={4} xl={3}>
+            <h3>{product.productname}</h3>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
-export default Homescreen
+export default Homescreen;
