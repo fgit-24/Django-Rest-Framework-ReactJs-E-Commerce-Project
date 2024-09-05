b52f52646da50c9838f76d759c2ce75b36c9cca1
Florian Hass <f91hama@gmail.com>
2024-09-05 15:12:38 +0000

    Set up productscreen.js

--- frontend/ecommerce/src/components/screens/HomeScreen.js
+++ frontend/ecommerce/src/components/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import axios from "axios";
-import { Row, Col, Card } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Product from "../Product";
 
 function HomeScreen() {
--- frontend/ecommerce/src/components/screens/ProductScreen.js
+++ frontend/ecommerce/src/components/screens/ProductScreen.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-function ProductScreen() {
+import { Link, useParams } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Container,
+} from "react-bootstrap";
+import Rating from "../Rating";
+import axios from "axios";
+
+function ProductScreen(params) {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const {id} = useParams()
+      const { data } = await axios.get(`/api/product/${id}`);
+      setProduct(data);
+    }
+    fetchProducts();
+  }, []);
   return (
-    <div>ProductScreen</div>
-  )
+    <Container>
+      <div>
+        <Link to="/" className="btn btn-dark my-3">
+          Go back
+        </Link>
+
+
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+      </Row>
+
+      </div>
+    </Container>
+  );
 }
 
-export default ProductScreen
+export default ProductScreen;
