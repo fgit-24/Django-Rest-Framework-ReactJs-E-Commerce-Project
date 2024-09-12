286f63cdd3ef4756026f6ef88e901b9f71905ec6
Florian Hass <f91hama@gmail.com>
2024-09-12 13:50:31 +0000

    Fix userprofiles url and serializer

--- backend/myenv/ecommerceproject/ecomapp/views.py
+++ backend/myenv/ecommerceproject/ecomapp/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 # from .products import products
 from .models import Products
-from .serializer import ProductSerializer, UserSerializer
+from .serializer import ProductsSerializer, UserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
