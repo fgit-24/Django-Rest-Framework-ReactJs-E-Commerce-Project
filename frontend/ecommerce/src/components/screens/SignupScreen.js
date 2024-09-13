d002bb2c8b151e7903c6f5ec094e660f27021703
Florian Hass <f91hama@gmail.com>
2024-09-13 11:24:34 +0000

    Add signup card to signupscreen.js

--- frontend/ecommerce/src/components/screens/SignupScreen.js
+++ frontend/ecommerce/src/components/screens/SignupScreen.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {userState, useEffect} from 'react';
+import {Container, Row, Col, Button, Form, Card} from 'react-bootstrap';
+import {Link, useNavigate, useLocation} from 'react-router-dom';
+import {useDispatch, useSelector} from "react-redux";
+import Loader from "../Loader"
+import Message from "../Message"
+
+
+
+
 
 function SignupScreen() {
   return (
-    <div>SignupScreen</div>
+<>
+<Container className='mt-3'>
+
+<Row>
+  <Col md={4}></Col>
+  <Col md={4}>
+  
+    <Card>
+      <Card.Header as="h1" classname='text-center bg-black text-light'>
+        Signup
+      </Card.Header>
+    </Card>
+  
+  
+  
+  </Col>
+  <Col md={4}></Col>
+</Row>
+
+</Container>
+</>
   )
 }
 
