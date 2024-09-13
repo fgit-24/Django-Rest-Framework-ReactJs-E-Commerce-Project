f16da1499fc35cd3111a9f140e0709cf25619183
Florian Hass <f91hama@gmail.com>
2024-09-13 11:36:32 +0000

    Add form to signup screen.js

--- frontend/ecommerce/src/components/screens/SignupScreen.js
+++ frontend/ecommerce/src/components/screens/SignupScreen.js
@@ -1,38 +1,54 @@
-import React, {userState, useEffect} from 'react';
-import {Container, Row, Col, Button, Form, Card} from 'react-bootstrap';
-import {Link, useNavigate, useLocation} from 'react-router-dom';
-import {useDispatch, useSelector} from "react-redux";
-import Loader from "../Loader"
-import Message from "../Message"
-
-
-
-
+import React, { userState, useEffect } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  Card,
+  FormControl,
+} from "react-bootstrap";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../Loader";
+import Message from "../Message";
 
 function SignupScreen() {
   return (
-<>
-<Container className='mt-3'>
+    <>
+      <Container className="mt-3">
+        <Row>
+          <Col md={4}></Col>
+          <Col md={4}>
+            <Card>
+              <Card.Header as="h3" classname="text-center bg-black text-light">
+                Signup
+              </Card.Header>
 
-<Row>
-  <Col md={4}></Col>
-  <Col md={4}>
-  
-    <Card>
-      <Card.Header as="h1" classname='text-center bg-black text-light'>
-        Signup
-      </Card.Header>
-    </Card>
-  
-  
-  
-  </Col>
-  <Col md={4}></Col>
-</Row>
-
-</Container>
-</>
-  )
+              <Card.Body>
+                <Form>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                  >
+                    <Form.Control as="textarea" rows={3} />
+                  </Form.Group>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4}></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default SignupScreen
+export default SignupScreen;
