7455571e3175e1b42e922d16c7941f3905b7c719
Florian Hass <f91hama@gmail.com>
2024-09-13 12:27:56 +0000

    Add input group to signup screen

--- frontend/ecommerce/src/components/screens/SignupScreen.js
+++ frontend/ecommerce/src/components/screens/SignupScreen.js
@@ -12,6 +12,7 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../Loader";
 import Message from "../Message";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function SignupScreen() {
   return (
@@ -27,13 +28,50 @@ function SignupScreen() {
 
               <Card.Body>
                 <Form>
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlInput1"
-                  >
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
+                  <Form.Group className="mb-3" controlId="fname">
+                    <Form.Label> <span><i className="fa fa-user"></i></span>First Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Your First Name" value="" re/>
                   </Form.Group>
+                  <Form.Group className="mb-3" controlId="lname">
+                    <Form.Label> <span><i className="fa fa-user"></i></span>Last Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Your Last Name" value="" re/>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="email">
+                    <Form.Label> <span><i className="fa-solid fa-envelope"></i></span>Email</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Your Email" value="" re/>
+                  </Form.Group>
+
+
+                  <Form.Group className="mb-3" controlId="pass1">
+                    <Form.Label> <span><i className="fa-solid fa-envelope"></i></span>Password</Form.Label>
+                     <InputGroup className="m-3">
+
+
+                                {" "}
+                                <Form.Control
+                                  placeholder="Enter a Password"
+                                  required
+                                  type="password"
+                                  id="pass1"
+                                  value={password}
+                                />
+
+                            
+                            </InputGroup>
+                            
+                    <small>Password must include at least [1-9][a-z][!-z][_$@*!..] and five Characters</small>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="pass2">
+                    <Form.Label> <span><i className="fa-solid fa-envelope"></i></span>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Your Password" value="" re/>
+                  </Form.Group>
+                  
+
+
+
+
+
+
                   <Form.Group
                     className="mb-3"
                     controlId="exampleForm.ControlTextarea1"
